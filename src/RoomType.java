e590f70208a4d2545182a4c5ed200a186368de7b
melsh <melsheikh053@gmail.com>
2026-04-21 17:55:13 +0200

    add RoomType

---
+++ src/RoomType.java
@@ -0,0 +1,60 @@
+public class RoomType {
+
+    private int typeId;
+    private String typeName;       
+    private int maxOccupancy;
+    private double basePricePerNight;
+
+    // Constructor
+    public RoomType(int typeId, String typeName, int maxOccupancy, double basePricePerNight) {
+        this.typeId = typeId;
+        this.typeName = typeName;
+        this.maxOccupancy = maxOccupancy;
+        this.basePricePerNight = basePricePerNight;
+    }
+
+    // Getters
+    public int getTypeId() {
+        return typeId;
+    }
+
+    public String getTypeName() {
+        return typeName;
+    }
+
+    public int getMaxOccupancy() {
+        return maxOccupancy;
+    }
+
+    public double getBasePricePerNight() {
+        return basePricePerNight;
+    }
+
+    // Setters
+    public void setTypeName(String typeName) {
+        this.typeName = typeName;
+    }
+
+    public void setMaxOccupancy(int maxOccupancy) {
+        if (maxOccupancy < 1) {
+            System.out.println("Error: max occupancy must be at least 1.");
+        } else {
+            this.maxOccupancy = maxOccupancy;
+        }
+    }
+
+    public void setBasePricePerNight(double basePricePerNight) {
+        if (basePricePerNight <= 0) {
+            System.out.println("Error: base price must be greater than 0.");
+        } else {
+            this.basePricePerNight = basePricePerNight;
+        }
+    }
+
+    public void printInfo() {
+        System.out.println("Type ID: " + typeId);
+        System.out.println("Type Name: " + typeName);
+        System.out.println("Max Occupancy: " + maxOccupancy);
+        System.out.println("Base Price Per Night: " + basePricePerNight + " EGP");
+    }
+}
